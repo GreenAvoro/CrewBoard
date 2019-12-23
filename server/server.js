4756762451192145eb0844bf104951f061a5920f
blackmage <haydos.shaw@gmail.com>
2019-12-24 11:45:31 +1300

    Heroku Commit

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ const Squad = require('./model/Squad')
 const Training = require('./model/Training')
 const Member = require('./model/Member')
 const cors = require('cors')
-const axios = require('axios')
 
 
 
@@ -46,6 +45,13 @@ app.get('/api/squads', async (req, res) => {
 
 })
 
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static('../client/build'))
+    app.get('*', (req, res ) => {
+        res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
+    })
+}
 
 
 const PORT = process.env.PORT || 5000
