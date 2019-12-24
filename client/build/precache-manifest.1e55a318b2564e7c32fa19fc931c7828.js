9192ab36719437f12e38b318262282d4223f0835
blackmage <haydos.shaw@gmail.com>
2019-12-24 13:01:47 +1300

    added concurrently

---
+++ client/build/precache-manifest.1e55a318b2564e7c32fa19fc931c7828.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "823dc8fdfd3da90f94062ef63e78cdc1",
+    "url": "/crewboard/index.html"
+  },
+  {
+    "revision": "f8cca1f8140add2508a5",
+    "url": "/crewboard/static/css/main.66fa14da.chunk.css"
+  },
+  {
+    "revision": "775c8b4d3e231e7bddb5",
+    "url": "/crewboard/static/js/2.aeeefe74.chunk.js"
+  },
+  {
+    "revision": "f8cca1f8140add2508a5",
+    "url": "/crewboard/static/js/main.2503ee9d.chunk.js"
+  },
+  {
+    "revision": "8600eceee53bc2bee212",
+    "url": "/crewboard/static/js/runtime-main.7a6d03bf.js"
+  },
+  {
+    "revision": "80914e2f700025f3a24b703e11ea2780",
+    "url": "/crewboard/static/media/events.80914e2f.svg"
+  },
+  {
+    "revision": "1c1f15ca0b7858157bfa97105d040262",
+    "url": "/crewboard/static/media/logo.1c1f15ca.png"
+  },
+  {
+    "revision": "fb82b6264366ad651b30d7c5c3f547b4",
+    "url": "/crewboard/static/media/regattas.fb82b626.svg"
+  },
+  {
+    "revision": "04e848f8f484417eea06b36b6874e01b",
+    "url": "/crewboard/static/media/settings.04e848f8.svg"
+  },
+  {
+    "revision": "8e478435a8256af7888673e121221beb",
+    "url": "/crewboard/static/media/trainings.8e478435.svg"
+  }
+]);
