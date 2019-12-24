fcd8a7e983e8cd748a06f6eeb064ab9f11fad571
blackmage <haydos.shaw@gmail.com>
2019-12-24 21:50:09 +1300

    Collapsable menu added to Media queries

--- client/src/components/SidePanel.js
+++ client/src/components/SidePanel.js
@@ -1,58 +1,62 @@
-import React from 'react'
+import React, {useState} from 'react'
 import TrainingsIcon from '../images/trainings.svg'
 import EventsIcon from '../images/events.svg'
 import RegattasIcon from '../images/regattas.svg'
 import SettingsIcon from '../images/settings.svg'
 
-function SidePanel(props){
+function SidePanel(props) {
 
     // const [squads, updateSquads] = useState([])
+    const [menuCollapse, updateMenuCollapse] = useState(true)
 
 
-    const squad_names = props.squads.map((squad,i) =>{
-        return <p 
-                    key={i} 
-                    onClick={() => {
-                        props.updateSquad(squad)
-                        props.panelUpdate("trainings")
-                    }}
-                    className={props.currentSquad === squad ? "list-active": ""}
-                >
-                    {squad}
-                </p>
+    const squad_names = props.squads.map((squad, i) => {
+        return <p
+            key={i}
+            onClick={() => {
+                props.updateSquad(squad)
+                props.panelUpdate("trainings")
+            }}
+            className={props.currentSquad === squad ? "list-active" : ""}
+        >
+            {squad}
+        </p>
     })
-    return(
+    return (
         <div className="side-panel">
-            <div className="side-panel-list">
-                <h2 className="active" onClick={() => {
-                    props.updateSquad("all")
-                    props.panelUpdate("trainings")
+            <div className="menu-button" onClick={() => updateMenuCollapse(prevState => !prevState)}>Menu &#9776;</div>
+            <div className={menuCollapse ? "menu-collapse" : "nothing"}>
+                <div className="side-panel-list">
+                    <h2 className="active" onClick={() => {
+                        props.updateSquad("all")
+                        props.panelUpdate("trainings")
                     }
-                }>
-                    <img src={TrainingsIcon} className="list-icon" alt="Trainiing Icon"></img>
-                    Trainings
+                    }>
+                        <img src={TrainingsIcon} className="list-icon" alt="Trainiing Icon"></img>
+                        Trainings
                 </h2>
-                <div className="side-panel-sublist">
-                    {squad_names}
+                    <div className="side-panel-sublist">
+                        {squad_names}
+                    </div>
                 </div>
-            </div>
 
-            <div className="side-panel-list">
-                <h2>
-                    <img src={RegattasIcon} className="list-icon" alt="Regattas Icon"></img>
-                    Regattas</h2>
-            </div>
+                <div className="side-panel-list">
+                    <h2>
+                        <img src={RegattasIcon} className="list-icon" alt="Regattas Icon"></img>
+                        Regattas</h2>
+                </div>
 
-            <div className="side-panel-list">
-                <h2>
-                    <img src={EventsIcon} className="list-icon" alt="Events Icon"></img>
-                    Events</h2>
-            </div>
+                <div className="side-panel-list">
+                    <h2>
+                        <img src={EventsIcon} className="list-icon" alt="Events Icon"></img>
+                        Events</h2>
+                </div>
 
-            <div className="side-panel-list">
-                <h2 onClick={() => props.panelUpdate("settings")}>
-                    <img src={SettingsIcon} className="list-icon" alt="Settings Icon"></img>
-                    Settings</h2>
+                <div className="side-panel-list">
+                    <h2 onClick={() => props.panelUpdate("settings")}>
+                        <img src={SettingsIcon} className="list-icon" alt="Settings Icon"></img>
+                        Settings</h2>
+                </div>
             </div>
         </div>
     )
